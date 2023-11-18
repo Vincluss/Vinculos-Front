2ea398ff4235ae08c2183931796fd8ca2fb374d3
MaikonHenriqu9 <maikonhenrique189@gmail.com>
2023-11-18 01:34:42 -0300

    página currículo

---
+++ src/app/curriculo/page.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Style from "@/src/css/curriculo.module.css"
+import Image from 'next/image';
+import onda from "@/src/assets/img-efeitos/onda_cima.svg"
+
+export default function Curriculo() {
+  return (
+    <section className={Style.container_curriculo}>
+        <Image src={onda} className={Style.onda}/>
+        
+        <div className={Style.container_centro}>
+            <h1 className={Style.titulo_curriculo}>Curriculo</h1>
+
+            <h1 className={Style.subtitulo_curriculo}>CRIE SEU CURRÍCULO</h1>
+
+            <button type="submit" className={Style.btn_enviarCurriculo}>BAIXAR MODELO DE CURRÍCULO</button>
+        </div>
+    </section>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import Vagas from "@/src/componentes/vagas"
+import Curriculo from './curriculo/page';
 
 
 export default function Home() {
   return (
     <>
-      <Vagas />
+      <Curriculo/>
     </>
-  )
+    
+  );
 }
 
 // Pasta page que está na pasta raiz é igual á pasta /
