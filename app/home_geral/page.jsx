2465ccf431269194eadb98d1bcf17a6e02023e59
gaabs16 <gbhernandes@hotmail.com>
2023-11-17 22:01:41 -0300

    add conteudo

--- app/home_geral/page.jsx
+++ app/home_geral/page.jsx
@@ -4,10 +4,16 @@ import OndaCima from '@/public/img/img-efeitos/onda_cima.svg'
 export default function HomeGeral() {
   return (
     <main>
-      <Image 
-        src={OndaCima}
-        alt="Onda"
-      />
+      <div>
+        <Image 
+          src={OndaCima}
+          alt="Onda"
+        />
+      </div>
+
+      <div>
+        
+      </div>
     </main>
   )
 }
