ab429b4c88a7e0048100903ea4e704fdc6bffdd1
MaikonHenriqu9 <maikonhenrique189@gmail.com>
2023-11-19 15:56:24 -0300

    atualização página cadastro empresa

--- src/app/cadastro_empresa/page.jsx
+++ src/app/cadastro_empresa/page.jsx
@@ -4,15 +4,26 @@ import Image from "next/image";
 
 export default function CadastroEmpresa(){
     return(
-        <section>
+        <section className={Style.paginaCadastro}>
             {/* Formulário */}
             <div className={Style.caixaFormulário}>
                 <h1>EMPRESA</h1>
+                <h1></h1>
             </div>
 
             {/* Saudação da página */}
             <div className={Style.caixaSaudacao}>
-                <Image src={logo}/>
+                <h1>BEM VINDO DE VOLTA!</h1>
+
+                <h2>
+                    para manter-se conectado <br/>
+                    conosco, faça login com seus <br/>
+                    dados pessoais
+                </h2>
+                <div className={Style.logo}>
+                    <Image src={logo} />
+                </div>
+                
             </div>
         </section>    
     );
