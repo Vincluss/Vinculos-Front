fe5ba3f7029e05c3d6dbee8ab1cd0377d447e176
gaabs16 <gbhernandes@hotmail.com>
2023-11-20 18:11:26 -0300

    add conteudo das homes

--- src/app/home_empresa/page.jsx
+++ src/app/home_empresa/page.jsx
@@ -1,1 +1,9 @@
+import HomeEmpresa from '@/src/componentes/home_empresa';
+import Link from 'next/link';
 
+
+export default function Empresa() {
+    return (
+        <HomeEmpresa />
+    )
+}
--- src/app/home_geral/layout.jsx
+++ src/app/home_geral/layout.jsx
@@ -9,7 +9,7 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Principal",
+  title: "Público",
   description:
     "A plataforma que emprega e capacita pessoas com deficiências sensoriais",
 };
