b0c357f60ed3ebf7a4ea797b343cff9196c6222e
Gustavo Saturnino <ga38185@gmail.com>
2023-11-22 12:08:02 -0300

    Iniciando Header

--- src/app/curriculo/page.jsx
+++ src/app/curriculo/page.jsx
@@ -1,22 +1,25 @@
-import Link from 'next/link';
-import Style from "@/src/css/curriculo.module.css"
-import Image from 'next/image';
+import Link from "next/link";
+import Style from "@/src/css/curriculo.module.css";
+import Image from "next/image";
 import onda from "@/src/assets/img-efeitos/onda_cima.svg";
+import Header from "@/src/componentes/header";
 
 export default function Curriculo() {
   return (
     <section className={Style.container_curriculo}>
-        <Image src={onda} className={Style.onda}/>
-        
-        <div className={Style.container_centro}>
-            <h1 className={Style.titulo_curriculo}>Curriculo</h1>
+      <Header />
+      
+      <Image src={onda} className={Style.onda} />
 
-            <h1 className={Style.subtitulo_curriculo}>CRIE SEU CURRÍCULO</h1>
+      <div className={Style.container_centro}>
+        <h1 className={Style.titulo_curriculo}>Curriculo</h1>
 
-            <button type="submit" className={Style.btn_enviarCurriculo}>BAIXAR MODELO DE CURRÍCULO</button>
-        </div>
+        <h1 className={Style.subtitulo_curriculo}>CRIE SEU CURRÍCULO</h1>
 
-        
+        <button type="submit" className={Style.btn_enviarCurriculo}>
+          BAIXAR MODELO DE CURRÍCULO
+        </button>
+      </div>
     </section>
-  )
+  );
 }
--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -3,14 +3,10 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <section className={Style.container__vagas}>
-      <h3 className={Style.title__vagas}>Vagas disponíveis</h3>
+    <header>
 
-      <Link href="/" className={Style.subtitle__link}>
-        <h4 className={Style.subTitle__vagas}>
-            Header
-        </h4>
-      </Link>
-    </section>
+      
+
+    </header>
   );
 }
