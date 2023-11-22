f34374a8d6cad3c7ddbfe0d34e3af197cce7ca54
Talisson Santos <talissonhenrique00@gmail.com>
2023-11-22 00:23:45 -0300

    ratings

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import Link from 'next/link'
-import Cursos from '../componentes/cursos/cursos'
+import Cursos_user from '../componentes/cursos/cursos_user'
 
 
 
@@ -10,14 +9,12 @@ import Cursos from '../componentes/cursos/cursos'
 export default function Home() {
   return (
     <>
-      <Cursos />
+      <Cursos_user />
      
    
     </>
   )
 }
 
-=======
->>>>>>> ea53d99c4652da41765f6d0f4fcc3aeab6338e3f
 // Pasta page que está na pasta raiz é igual á pasta /
 // Página de renderização - Home
