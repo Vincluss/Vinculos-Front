e4340386c4e1d77a18472dafac0f913569bb55b0
Gustavo Saturnino <ga38185@gmail.com>
2023-11-23 00:45:38 -0300

    Add Userway Script

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -1,5 +1,4 @@
 import Pagamento from "@/src/componentes/pagamento";
-
 export default function Home(){
     return(
         <>
--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
+    <header className="">
 
-      
+
 
     </header>
   );
