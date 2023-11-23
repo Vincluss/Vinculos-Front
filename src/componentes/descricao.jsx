20fc6990814ab1acd2ad7b2f6a541511554d545b
Lauragamer1326 <141660841+Lauragamer1326@users.noreply.github.com>
2023-11-22 22:56:29 -0300

    Add files via upload

---
+++ src/componentes/descricao.jsx
@@ -0,0 +1,14 @@
+import Style from "../css/descriçao_vagas.module.css";
+
+export default function Descriçao() {
+    return(
+        <section className={Style.container}>
+        <div className={Style.caixa1}>
+
+        </div>
+        <div className={Style.caixa3}></div>
+        <p className={Style.caixa4}>Administrador Banco de Dados III - Sql Server - Home Office</p>
+        
+        </section>
+    );
+}
