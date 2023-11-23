40e992167c6957bd724250662feb3178aaa4f1cc
Joyce Camille <joyce.ssilva02@gmail.com>
2023-11-22 22:58:25 -0300

    pagina de login empresa cadastro do usuario

---
+++ src/app/login_empresa/page.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Loginempresa from '@/src/componentes/login_empresa';
+
+
+export default function Home() {
+    return (
+        <>
+        <Loginempresa />
+        </>
+    )
+}
