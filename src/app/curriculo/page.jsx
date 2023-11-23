a71a544b31c974552aa26f589b482287f10db8b0
Gustavo Saturnino <ga38185@gmail.com>
2023-11-23 02:19:47 -0300

    Fix page cursos

--- src/app/curriculo/page.jsx
+++ src/app/curriculo/page.jsx
@@ -3,12 +3,13 @@ import Style from "@/src/css/curriculo.module.css";
 import Image from "next/image";
 import onda from "@/src/assets/img-efeitos/onda_cima.svg";
 import Footer from "@/src/componentes/Footer";
+import Wave from "@/src/componentes/wave";
 
 export default function Curriculo() {
   return (
     <section className={Style.container_curriculo}>
       <Header />
-
+      <Wave />
       <Image src={onda} className={Style.onda} />
 
       <div className={Style.container_centro}>
--- src/componentes/inclusao_home.jsx
+++ src/componentes/inclusao_home.jsx
@@ -1,30 +1,23 @@
 import Image from "next/image";
 import Style from "../css/inclusao_home.module.css";
-import Inclusao from '@/src/assets/img-pessoas/img-inclusao1.png'
-import Inclusao1 from '@/src/assets/img-pessoas/img-inclusao2.png'
-import Inclusao2 from '@/src/assets/img-pessoas/img-inclusao3.png'
+import Inclusao from "@/src/assets/img-pessoas/img-inclusao1.png";
+import Inclusao1 from "@/src/assets/img-pessoas/img-inclusao2.png";
+import Inclusao2 from "@/src/assets/img-pessoas/img-inclusao3.png";
 
-export default function Candidatos(){
-    return (
-        <div className={Style.fundo_inclusao}>
-            <div className={Style.fundo3}>
-                <h3 className={Style.titulo}>Inclusão</h3>
-                <div className={Style.imagens}>
-                    <Image className={Style.inclusao1}
-                    src={Inclusao}
-                    alt="inclusao"
-                    />
-                    <Image className={Style.inclusao2}
-                    src={Inclusao1}
-                    alt="inclusao1"
-                    />
-                    <Image className={Style.inclusao3}
-                    src={Inclusao2}
-                    alt="inclusao2"
-                    />
-                </div>
-                <button className={Style.botao3}>Ver Mais</button>
-            </div>
+export default function Candidatos() {
+  return (
+    <div className={Style.fundo_inclusao}>
+      <div className={Style.fundo3}>
+        <h3 className={Style.titulo}>Inclusão</h3>
+        <div className={Style.imagens}>
+          <Image className={Style.inclusao1} src={Inclusao} alt="inclusao" />
+          <Image className={Style.inclusao2} src={Inclusao1} alt="inclusao1" />
+          <Image className={Style.inclusao3} src={Inclusao2} alt="inclusao2" />
         </div>
-    )
+        <div className={Style.button__container}>
+          <button className={Style.botao3}>Ver Mais</button>
+        </div>
+      </div>
+    </div>
+  );
 }
