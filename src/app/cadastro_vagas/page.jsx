f45573eb09cd56aab087a74d61b293a3e2fd2e6c
user_acronus <user_acronus@senacsp.edu.br>
2023-11-23 08:57:41 -0300

    update page forms

--- src/app/cadastro_vagas/page.jsx
+++ src/app/cadastro_vagas/page.jsx
@@ -1,54 +1,57 @@
 import Footer from "@/src/componentes/footer";
+import Header from "@/src/componentes/header";
 import Style from "@/src/css/cadastrar_vagas.module.css"
 
 export default function CadastrarVagas(){
     return(
         <section className={Style.cadastrarVagas}>
+            <Header/>
 
-            <form className={Style.containerForms}>
-                    <label htmlFor="vaga" className={Style.tituloCadastro}>Cadastrar Vaga</label>
+            <div className={Style.containerForms}>
+                <form className={Style.forms}>
+                        <label htmlFor="vaga" className={Style.tituloCadastro}>Cadastrar Vaga</label>
 
-                    <input type="text" id="vaga" name="vaga" placeholder="Nome Da Vaga" className={Style.estilizacaoInput}/>
+                        <input type="text" id="vaga" name="vaga" placeholder="Nome Da Vaga" className={Style.estilizacaoInput}/>
 
-                    <label htmlFor="" className={Style.subTitulo}>Área De Atuação</label>
+                        <label htmlFor="" className={Style.subTitulo}>Área De Atuação</label>
+                        <div className={Style.posicionarCheckBox}>
+                            <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
+                            <datalist id="areas_atuacao">
+                                <option value="Administração"></option>
+                                <option value="Desenvolvedor Junior"></option>
+                                <option value="Financeiro"></option>
+                            </datalist>
+                            <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
+                            <datalist id="areas_atuacao">
+                                <option value="Administração"></option>
+                                <option value="Desenvolvedor Junior"></option>
+                                <option value="Financeiro"></option>
+                            </datalist>
+                            <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
+                            <datalist id="areas_atuacao">
+                                <option value="Administração"></option>
+                                <option value="Desenvolvedor Junior"></option>
+                                <option value="Financeiro"></option>
+                            </datalist>
+                        </div>                    
+                        
+                        <label htmlFor="" className={Style.subTitulo}>Modelo De Trabalho</label>
                     <div className={Style.posicionarCheckBox}>
-                        <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
-                        <datalist id="areas_atuacao">
-                            <option value="Administração"></option>
-                            <option value="Desenvolvedor Junior"></option>
-                            <option value="Financeiro"></option>
-                        </datalist>
-                        <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
-                        <datalist id="areas_atuacao">
-                            <option value="Administração"></option>
-                            <option value="Desenvolvedor Junior"></option>
-                            <option value="Financeiro"></option>
-                        </datalist>
-                        <input list="areas_atuacao" placeholder="Selecione Uma Tag" className={Style.estilizacaoTags}/>
-                        <datalist id="areas_atuacao">
-                            <option value="Administração"></option>
-                            <option value="Desenvolvedor Junior"></option>
-                            <option value="Financeiro"></option>
-                        </datalist>
-                    </div>                    
                     
-                    <label htmlFor="" className={Style.subTitulo}>Modelo De Trabalho</label>
-                    <div className={Style.posicionarCheckBox}>
-                
                         <label htmlFor="presencial" className={Style.estilizacaoCheckBox}>Presencial
                             <input type="checkbox" name="presencial" id="presencial" value="presencial" />
                         </label>
-                        
+                            
                         <label htmlFor="presencial" className={Style.estilizacaoCheckBox}>Híbrido
                             <input type="checkbox" name="presencial" id="presencial" value="presencial"/>
                         </label>
-                        
+                            
                         <label htmlFor="presencial" className={Style.estilizacaoCheckBox}>Remoto
                             <input type="checkbox" name="presencial" id="presencial" value="presencial"/>
                         </label>
                     </div>
-                    
-                    
+                        
+                        
                     <input list="estado" placeholder="Insira Aqui Seu Estado" className={Style.estilizacaoInput}></input>
 
 
@@ -58,9 +61,10 @@ export default function CadastrarVagas(){
                     <input type="number" name="" id="" placeholder="Valor Do Salário" min="0" className={Style.estilizacaoSalario}/>
 
 
-            </form>
+                </form>
 
-            <button type="submit" className={Style.btn_cadastrar}>CADASTRAR</button>
+                <button type="submit" className={Style.btn_cadastrar}>CADASTRAR</button>
+            </div>            
 
             <Footer/>
         </section>    
