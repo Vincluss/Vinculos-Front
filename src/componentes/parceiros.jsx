7e2d4f7c9899aaed4488f081b775468412c90865
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2023-11-26 17:31:18 -0300

    Alteração em Parceiros.jsx

--- src/componentes/parceiros.jsx
+++ src/componentes/parceiros.jsx
@@ -1,11 +1,35 @@
-import Style from "../css/parceiros.module.css"
+import Style from "../css/parceiros.module.css";
+import Image from "next/image";
+
+const Partners = ({ image }) => {
+    return (
+        <Image
+            src={image}
+            alt="Parceiro"
+            priority
+        />
+    )
+}
 
 export default function Parceiros(){
+    const partnerImages = [
+        { image: '' },
+        { image: '' }
+    ]
+
     return (
         <div className={Style.fundo1}>
             <h1 className={Style.frase2}>Parceiros</h1>
-
-            {/* carrossel de parceiros */}
+            <div className={Style.logos}>
+                <div className={Style.logosSlide}>
+                    {partnerImages.map((images, index) => {
+                        <Partners
+                            key={index}
+                            {...images}
+                        />
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
