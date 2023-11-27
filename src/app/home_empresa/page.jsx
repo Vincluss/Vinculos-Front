55920752b5b97c5f710e8b44adce069c59c519e7
gaabs16 <gbhernandes@hotmail.com>
2023-11-27 02:17:30 -0300

    headers

--- src/app/home_empresa/page.jsx
+++ src/app/home_empresa/page.jsx
@@ -3,15 +3,14 @@ import AssinaturaHome from '@/src/componentes/assinatura_home';
 import HomeEmpresa from '@/src/componentes/home_empresa';
 import Link from 'next/link';
 import Header from '@/src/componentes/header_publico';
-import Wave from '@/src/componentes/wave';
-import HomeGeral from '@/src/componentes/home_geral';
+import WaveEmpresa from '@/src/componentes/wave_empresa';
 
 export default function Empresa() {
     return (
         <>
             <header className='relative'>
                 <Header bgColor="#243B53"/>
-                <Wave />
+                <WaveEmpresa />
             </header>
             <HomeEmpresa/>
             <AssinaturaHome />
--- src/componentes/home_empresa.jsx
+++ src/componentes/home_empresa.jsx
@@ -1,7 +1,6 @@
 'use client'
 import Image from "next/image";
 import Style from "../css/home_empresa.module.css"
-import Comeco from '@/src/assets/img-efeitos/cima_escuro.png'
 import ImageMain from "@/src/assets/img-efeitos/blob.png"
 import Link from "next/link"
 
@@ -11,17 +10,17 @@ export default function HomeEmpresa() {
             <section className="relative">
                 <div className="bg-main-empresa w-full h-[90vh] mt-[15vh] flex items-end justify-between relative">
 
-                    <div className="w-5/12 flex items-baseline relative h-5/6">
-                        <Image src={ImageMain} alt="Código" className="absolute z-10 w-full h-full" />
+                    <div className="w-6/12 flex items-baseline relative h-300">
+                        <Image src={ImageMain} alt="Código" className="z-10 w-full h-full" />
                     </div>
 
-                    {/* <div className="w-2/5 pb-8 pr-8">
+                    <div className={Style.FormularioEmpresa}>
                         <h1 className={Style.frase3}>Cadastre Suas Vagas</h1>
                         <form className={Style.formulario1}>
                             <input className={Style.email1} type="text" placeholder="insira seu email" name="name" />
                             <button className={Style.botao3} type="submit">Enviar</button>
                         </form>
-                    </div> */}
+                    </div>
                 </div>
             </section>
 
