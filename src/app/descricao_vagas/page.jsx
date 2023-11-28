59ec869294feaf8a88b65c25d1b4a666fee58995
Gustavo Saturnino <ga38185@gmail.com>
2023-11-28 15:39:38 -0300

    add wave and position

--- src/app/descricao_vagas/page.jsx
+++ src/app/descricao_vagas/page.jsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import DescricaoVagas from "@/src/componentes/descricao";
+import Wave from "@/src/componentes/wave";
+import Header from "@/src/componentes/header_publico";
 
 export default function Home() {
   return (
     <>
+      <header className="relative">
+        <Header bgColor={"#003786"} />
+        <Wave bgColor="#003786" position= "relative" />
+      </header>
       <DescricaoVagas />
-
     </>
   );
 }
