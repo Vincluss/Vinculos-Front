9a75395534c77f538805f0d120a8f7e529e51fba
MaikonHenriqu9 <maikonhenrique189@gmail.com>
2023-11-28 20:47:18 -0300

    página descrição vagas

--- src/componentes/descricao_vagas.jsx
+++ src/componentes/descricao_vagas.jsx
@@ -1,34 +1,13 @@
-import Style from "../css/descricao_vagas.module.css";
+import Style from "@/src/css/descricao_vaga.module.css"
 
-export default function Descricao_Vagas() {
+export default function DescricaoVagas() {
   return (
-    <section className={Style.container}>
-      Descrição: <br /> <br />
-      Administrar banco de dados em ambiente Azure Cloud; <br /> <br />
-      Criando e desenvolvendo objetos, tabelas, procedures, views, triggers,
-      jobs, e pacotes de SSIS.; <br /> <br />
-      Manutenção das bases em SQL Server e PostgreSQL; <br /> <br />
-      Construindo otimização de queries, índices e objetos de banco de dados e
-      Infraestrutura; Requisitos: <br />
-      Experiência com Metodologia Itil e de Gestão de Projetos (PMI e Agile);{" "}
-      <br /> <br />
-      Conhecimento em Administração de Banco de Dados <br /> <br />
-      SQL Server: Instalação, Gerenciamento de Acessos, Backup, Restore, <br />
-      Alertas, Monitoramento, Performance, Suporte a Equipe de negócios; <br />{" "}
-      <br />
-      Perfil pró-ativo, excelente comunicação; 
+    <section className={Style.containerDescricaoVaga}>
+      <h1 className={Style.titulo}>Descrição Da Vaga</h1>
 
-      Empresa: <br />
-      A Johnson Controls é uma
-      empresa multi-industrial global, líder em tecnologia diversificada, que <br />
-      atende a diversos clientes em mais de 150 países. Somos uma empresa que <br /> <br />
-      valoriza o diferente. Se você se identifica com alguma etnia, diversidade <br />
-      de gênero ou se é um profissional com deficiência, lhes damos as
-      boas-vindas!!! <br /> <br />
-      Queremos você para fazer a diferença em nosso time! <br /> 
-      Candidate-se as nossas vagas para construirmos juntos um ambiente mais
-      colorido e diverso! 
-
+      <p>
+        Mesias alsdçasnfka
+      </p>
     </section>
   );
 }
