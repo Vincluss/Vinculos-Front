678fed1f36fc22ce7aea672e4278f811c92d75cc
Renan Ladislau <renanladislau9@gmail.com>
2023-11-28 17:25:39 -0300

    Alterando a Sobre

---
+++ src/componentes/pilares.jsx
@@ -0,0 +1,26 @@
+import Style from "@/src/css/pilares_vinculos.module.css"
+
+
+
+
+export default function pilares(){
+    return(
+
+        <section className={Style.Pilares}> {/*section com os pilares*/}
+            <div> 
+                <div>
+                </div>
+            </div>
+
+
+
+
+
+        </section>
+
+
+
+    )
+
+
+}
