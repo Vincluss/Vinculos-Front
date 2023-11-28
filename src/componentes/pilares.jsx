b0c490a03dd787c237caf8018c020ac45443c8f8
Renan Ladislau <renanladislau9@gmail.com>
2023-11-28 19:19:12 -0300

    Alterando a Sobre novamente

--- src/componentes/pilares.jsx
+++ src/componentes/pilares.jsx
@@ -3,12 +3,38 @@ import Style from "@/src/css/pilares_vinculos.module.css"
 
 
 
-export default function pilares(){
-    return(
+export default function pilares() {
+    return (
 
         <section className={Style.Pilares}> {/*section com os pilares*/}
-            <div> 
-                <div>
+            <div className={Style.Mission}>
+                <div className={Style.SimboloMission}>
+                </div>
+                <div className={Style.TextoMission}>
+                    Facilitar oportunidades e promover a inclusão de pessoas com deficiência sensorial no mercado de trabalho.
+                </div>
+            </div>
+
+            <div className={Style.Vision}>
+                <div className={Style.SimboloVision}>
+                </div>
+                <div className={Style.TextoVision}>
+                    Criando um ambiente de trabalho onde todas as habilidades são valorizadas, respeitadas e aproveitadas para impulsionar a inovação e o sucesso coletivo.
+                </div>
+            </div>
+
+            <div className={Style.Value}>
+                <div className={Style.SimboloValue}>
+                </div>
+                <div className={Style.TextoValue}>
+                    <br></br>
+                    Inclusão e Diversidade: Comprometemo-nos a criar um ambiente de trabalho que celebre a diversidade em todas as suas formas.
+                    <br></br>
+                    <br></br>
+                    Igualdade de Oportunidades: Acreditamos em proporcionar igualdade de oportunidades para todos os membros da nossa equipe.
+                    <br></br>
+                    <br></br>
+                    Respeito e Empatia: Cultivamos um ambiente baseado no respeito mútuo e na empatia, onde cada pessoa é tratada com dignidade e compreensão.
                 </div>
             </div>
 
