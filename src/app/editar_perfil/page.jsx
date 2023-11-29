90dae9504fb345b76536b80ada688cb2ece32eae
Renan Ladislau <renanladislau9@gmail.com>
2023-11-29 12:13:38 -0300

    Arrumei o Problema da Gabs

--- src/app/editar_perfil/page.jsx
+++ src/app/editar_perfil/page.jsx
@@ -1,8 +1,8 @@
-import Footer from "@/src/componentes/footer";
 import Candidato from "@/src/componentes/formulario";
 import Header from "@/src/componentes/header_publico";
 import Perfil from "@/src/componentes/perfilCandidato";
 import Wave from "@/src/componentes/wave";
+import Footer from "@/src/componentes/footer";
 
 export default function EditarPerfil() {
   return (
@@ -11,8 +11,11 @@ export default function EditarPerfil() {
         <Header bgColor="#003786" />
         <Wave position={"relative"} bgColor="#003786" />
       </header>
+
+            
       <Perfil />
       <Candidato />
+      <Footer />
     </>
   );
 }
