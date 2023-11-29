4bcac513ffbb5de0f288854f35aaa832c3237b8e
gaabs16 <gbhernandes@hotmail.com>
2023-11-29 02:47:39 -0300

    doacao arrumando

--- src/app/doacao/page.jsx
+++ src/app/doacao/page.jsx
@@ -2,11 +2,15 @@ import Link from 'next/link';
 import Doe from '@/src/componentes/doacao_vinculos';
 import Footer from '@/src/componentes/footer';
 import Header from '@/src/componentes/header_publico';
+import Wave from '@/src/componentes/wave';
 
 export default function Home() {
     return (
         <>
-        <Header/>
+        <header className="relative">
+            <Header bgColor="#E8A023" />
+            <Wave position={"relative"} bgColor="#E8A023"/>
+        </header>
         <Doe />
         <Footer />
         </>
