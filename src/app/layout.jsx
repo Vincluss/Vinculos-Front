331f10613633079373cb0a138f1e9720862b89f1
Gustavo Saturnino <ga38185@gmail.com>
2023-11-29 15:28:51 -0300

    userway added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 import "@/src/app/globals.css";
-import Script from "next/script"
+import Script from "next/script";
 
 // A página layout é a página onde nosso código será renderizado.
 // Ela serve como uma página mãe para outras páginas
@@ -18,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-            {/* <Script src="https://cdn.userway.org/widget.js" data-account="0PWSHa9uTA"/> */}
+      <Script
+        src="https://cdn.userway.org/widget.js"
+        data-account="0PWSHa9uTA"
+      />
       <body className={poppins.className}>{children}</body>
     </html>
   );
