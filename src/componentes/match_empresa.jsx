4a2e7cd6b372a07c377640d7d65e594db433612a
MatheusJunior2334 <matheusjuniormjg2334@gmail.com>
2023-11-29 15:33:08 -0300

    Adição do conteúdo em match

--- src/componentes/match_empresa.jsx
+++ src/componentes/match_empresa.jsx
@@ -1,52 +1,73 @@
+'use client'
+
+import React, { useState } from "react";
 import Image from "next/image";
 import Style from "../css/match_empresa.module.css";
 import Dani from "@/public/img/img-pessoas/img-inclusao1.png"
 import Gus from "@/public/img/img-pessoas/img-inclusao2.png"
 import Danilo from "@/public/img/img-pessoas/img-inclusao3.png"
 
-export default function Candidatos() {
+const CandidatosProps = ({ imagemCandidato, nomeCandidato, matchPorcentagem }) => {
   return (
-  <article>
-    <h1 className={Style.Titulo}> Candidatos </h1>
+    <div className={Style.candidatosContent}>
+      <div className={Style.candidatosImagem}>
+        <Image
+          src={imagemCandidato}
+          alt={nomeCandidato}
+          priority
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className={Style.candidatosInfo}>
+        <h3>{nomeCandidato}</h3>
+        <p>Possui Um Match de {matchPorcentagem}% Com Sua Vaga De Estágio</p>
+        <button className={Style.saibaMais}>Saiba Mais</button>
+      </div>
+    </div>
+  )
+}
 
-    <section className={Style.Candidato}>
-        <div className={Style.Position}> {/* aqui displayflex normal */}
-          <Image src={Dani} alt="Candidata" className={Style.Ajusteimg}/>
+export default function Candidatos() {
+  const [mostrarTodos, setMostrarTodos] = useState(false);
+  const [displayText, setDisplayText] = useState('Ver Mais');
+
+  const vejaMais = () => {
+    setMostrarTodos(!mostrarTodos);
+    setDisplayText(mostrarTodos ? 'Ver Mais' : 'Ocultar')
+  }
+
+  const candidatos = [
+    { imagemCandidato: Dani, nomeCandidato: 'Daniele Almeida', matchPorcentagem: '50' },
+    { imagemCandidato: Gus, nomeCandidato: 'Gustavo Renato', matchPorcentagem: '70' },
+    { imagemCandidato: Danilo, nomeCandidato: 'Danilo Santos', matchPorcentagem: '80' },
+    
+    { imagemCandidato: Dani, nomeCandidato: 'Daniele Almeida', matchPorcentagem: '50' },
+    { imagemCandidato: Gus, nomeCandidato: 'Gustavo Renato', matchPorcentagem: '70' },
+    { imagemCandidato: Danilo, nomeCandidato: 'Danilo Santos', matchPorcentagem: '80' },
+  ]
+
+  const dividirCandidatos = mostrarTodos ? candidatos : candidatos.slice(0, 3);
+
+
+  return (
+    <section className={Style.section}>
+
+      <div className={Style.candidatosContainer}>
+        <div className={Style.Titulo}>
+          <h2>Candidatos</h2>
         </div>
 
-        <div className={Style.NomeMatch}>   {/*  aqui fica com displayflex de coluna */}
-          <h2 className={Style.HDois}> Daniele Almeida </h2>
-          <p> Possui um match de 50% com a sua vaga de estagio em Administração </p>
-          <button className={Style.SaibaMais}> Saiba Mais </button>
-        </div>
+        {dividirCandidatos.map((candidato, index) => (
+          <CandidatosProps
+            key={index}
+            {...candidato}
+          />
+        ))}
+      </div>
+
+      <button className={Style.VejaMais} onClick={vejaMais}>{displayText} Candidatos</button>
+
     </section>
-
-    <section className={Style.Candidato}>
-        <div className={Style.Position}> {/* aqui displayflex normal */}
-          <Image src={Gus} alt="Candidato" className={Style.Ajusteimg}/>
-        </div>
-
-        <div className={Style.NomeMatch}>   {/*  aqui fica com displayflex de coluna */}
-          <h2 className={Style.HDois}> Gustavo Renato </h2>
-          <p> Possui um match de 70% com a sua vaga de estagio em Administração </p>
-          <button className={Style.SaibaMais}> Saiba Mais </button>
-        </div>
-    </section>
-
-    <section className={Style.Candidato}>
-        <div className={Style.Position}> {/* aqui displayflex normal */}
-          <Image src={Danilo} alt="Candidato Danilo" className={Style.Ajusteimg}/>
-        </div>
-
-        <div className={Style.NomeMatch}>   {/*  aqui fica com displayflex de coluna */}
-          <h2 className={Style.HDois}> Danilo Santos </h2>
-          <p> Possui um match de 80% com a sua vaga de estagio em Administração </p>
-          <button className={Style.SaibaMais}> Saiba Mais </button>
-        </div>
-    </section>
-
-    <button className={Style.VejaMais}> Ver Mais Candidatos </button>
-
-  </article>
   );
 }
