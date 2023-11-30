915dfffd91e9d3111f4fe34a2008b3a49affb0e0
Wendel Harrison] <wendel.wdhr@gmail.com>
2023-11-29 22:12:20 -0300

    Modal de pagamento ok

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -6,8 +6,13 @@ import Pagamento from "@/src/componentes/pagamento";
 import Wave from "@/src/componentes/wave";
 import VLibras from "@/src/componentes/vlibras";
 
+import Visa from "./assets/visa.png"
+import Paypal from "./assets/paypal.png"
+import Master from "./assets/mastercard.png"
+
 
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home(){
     const [abrirModal, setAbrirModal] = useState(false);
@@ -35,9 +40,27 @@ return (
 
             <h1 className="uppercase text-center font-bold mt-2 mb-5 text-2xl">pagamento</h1>
 
-            <div className="bg-[#243B53] w-[80%] h-[65vh] rounded-md">
+            <div className="bg-[#6689B1] w-[80%] h-[65vh] rounded-md flex flex-col items-center">
 
                 <h3 className="text-center font-semibold mt-10 text-lg text-white">Cartão</h3>
+
+                <div className="flex justify-between gap-14 items-center h-10 mt-10 mb-14 ">
+                    <Image src={Visa} className="h-20 w-auto hover:scale-110 duration-100" />
+                    <Image src={Master} className="h-14 w-auto hover:scale-105 duration-100" />
+                    <Image src={Paypal} className="h-20 w-auto hover:scale-110 duration-100" />
+                </div>
+
+                <div className="w-4/5 ">
+                    <input type="text" className=" pl-5 py-4 mb-5 w-full rounded-md" placeholder="Nome do impresso no cartão" />
+                    <input type="tel" className=" pl-5 py-4 mb-5 w-full rounded-md" placeholder="Número do cartão"  />
+                    <div className="flex w-full justify-between">
+                        <input type="month" className=" px-5 py-4 w-[47.5%] rounded-md" />
+                        <input type="password" className=" px-5 py-4 w-[47.5%] rounded-md" placeholder="CVV" />
+                    </div>
+
+                </div>
+
+                <button className="bg-[#E8A023] w-4/5 py-3 text-white font-semibold hover:translate-y-1 duration-150 rounded-md mt-20">Pagar</button>
 
             </div>
         </Pagamento>
--- src/componentes/pagamento.jsx
+++ src/componentes/pagamento.jsx
@@ -8,7 +8,7 @@ return (
         <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
         <div className=" fixed w-[50vw] h-[85vh] bg-white rounded-md z-50 flex flex-col items-center justify-center">
             <button
-                className="bg-red-500 absolute top-0 right-0 py-2 px-4 rounded-md"
+                className="bg-red-500 absolute top-1 right-1 py-1 px-3 rounded-md text-white text-3xl"
                 onClick={fechar}
             >
             X
